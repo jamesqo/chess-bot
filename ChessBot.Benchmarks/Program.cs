ceacdbe38cfd0a45c12d0b4a41ef6b969d040296
James Ko <jamesqko@gmail.com>
2020-06-19 11:55:59 -0400

    Add benchmarks project

---
+++ ChessBot.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace ChessBot.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<SearchBenchmarks>();
+        }
+    }
+}
---
+++ ChessBot.Benchmarks/SearchBenchmarks.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+using ChessBot.Search;
+
+namespace ChessBot.Benchmarks
+{
+    [SimpleJob(RuntimeMoniker.NetCoreApp31, baseline: true)]
+    //[SimpleJob(RuntimeMoniker.Net48)]
+    [RPlotExporter]
+    public class SearchBenchmarks
+    {
+        [Params(5, 6, 7)]
+        public int Depth;
+
+        [Params(1 << 8, 1 << 12, 1 << 16)]
+        public int TtCapacity;
+
+        // todo: test on states besides startpos
+
+        [Benchmark]
+        public void Mtdf()
+        {
+            new Mtdf(TtCapacity)
+            {
+                Depth = Depth
+            }
+            .Search(State.Start);
+        }
+
+        [Benchmark]
+        public void MtdfIds()
+        {
+            new MtdfIds(TtCapacity)
+            {
+                Depth = Depth
+            }
+            .Search(State.Start);
+        }
+    }
+}
