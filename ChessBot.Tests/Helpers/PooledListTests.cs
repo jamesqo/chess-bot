cbae2a1279952b0e3a2fff8776966b7c14cc1969
James Ko <jamesqko@gmail.com>
2020-06-30 01:46:26 -0400

    Add regression test for earlier bug

---
+++ ChessBot.Tests/Helpers/PooledListTests.cs
@@ -0,0 +1,19 @@
+﻿using ChessBot.Helpers;
+using Xunit;
+
+namespace ChessBot.Tests.Helpers
+{
+    public class PooledListTests
+    {
+        [Fact]
+        public void Add_ExceedsCapacity_ResizesProperly()
+        {
+            using var list = PooledList<int>.Get(16);
+
+            for (int i = 0; i < 100; i++) list.Add(i);
+
+            Assert.Equal(100, list.Count);
+            for (int i = 0; i < 100; i++) Assert.Equal(i, list[i]);
+        }
+    }
+}
